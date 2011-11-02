1dac5aa260ce1a7b51931252f18a4ad4e7c951f5
david dellatore <dellatore@gmail.com>
2011-11-02 11:16:03 +0700

    first commit

---
+++ bash/aliases.sh
@@ -0,0 +1,10 @@
+alias reload=". $HOME/.profile" # same as previous, after editing you have to source it for the new stuff to work add that one line to the aliases.sh
+
+# standard
+alias l="ls -AF"
+alias la="ls -aliF"
+alias ls="ls -h --group-directories-first --color=auto"
+alias ll="ls -lFh"
+alias myip="curl --silent http://tnx.nl/ip"
+alias path='echo -e "${PATH//:/\n}"'
+
